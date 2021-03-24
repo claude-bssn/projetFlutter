e0aec43ab46286e240627856d5c0803fa3aa707a
Sara ZOUIOULA <sara.zouioula.17@eigsi.fr>
2021-03-24 17:17:44 +0100

    detail

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -2,15 +2,17 @@ import 'package:dwm14/components/drawer.dart';
 import 'package:dwm14/models/movie.dart';
 import 'package:flutter/material.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
   Widget build(BuildContext context) {
     final ItemArguments args = ModalRoute.of(context).settings.arguments;
     return Scaffold(
         appBar: AppBar(
-          title: Text('Detail'),
-          actions: [
-            Icon(Icons.edit),
-          ],
+          title: Text('Movie Detail'),
         ),
         drawer: DrawerMenu(),
         body: SafeArea(child: Text(args.text)));
